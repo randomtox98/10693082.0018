afb2950e9c5568ef896538a3d0bfc2d555ea53af
clement.vitrant <clement.vitrant@104f09.iut-lens.univ-artois.fr>
2018-02-19 16:56:47 +0100

    front page data loading

--- client/Home.js
+++ client/Home.js
@@ -7,15 +7,38 @@ import 'react-images-uploader/font.css';
 
 import {HTTP_SERVER_PORT_IMAGES} from '../server/constants';
 
+let idx=0;
+
+
+
+class CityLaconic extends React.Component {
+    render() {
+        return (
+            <div className='card'>
+                <img src={this.props.cities.picture}/>
+                <p>{this.props.cities.name}</p>
+            </div>
+        )
+    }
+}
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
+
     }
+    loadData(){
+        fetch('/cities')                       // Ask the route /cities to the server
+            .then(res => res.json())                       // Retrieve the objects  in json
+            .then(data => this.setState({cities: data}))   // Modify the state accordingly
+            .catch(err => console.log(err));               // Bad news: an error!
+};
 
     render() {
+        const tab = this.state.cities.map(p=> <CityLaconic cities={p}/>)
         return (
             <div>
+                {tab}
                 <h1>My Cities... The places to be!test</h1>
                 <p> You can find in this website many cities with beautiful places, events (festivals, concerts and so on).
                     Please, join us, and you will have the possibilities to participate to this new social network. <br />
@@ -25,3 +48,9 @@ export default class Home extends React.Component {
         );
     }
 }
+
+
+
+
+
+
