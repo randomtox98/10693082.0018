6f300264bd1b37fada79b4284a7dcdd6e88e1c61
julien.aballea <julien.aballea@104f05.iut-lens.univ-artois.fr>
2018-02-20 15:39:41 +0100

    citypage

--- client/City.js
+++ client/City.js
@@ -7,6 +7,18 @@ import 'react-images-uploader/font.css';
 
 import {HTTP_SERVER_PORT_IMAGES} from '../server/constants'
 
+class Activ extends React.Component{
+    render(){
+        return(
+        <div className='activities'>
+            <img src= {this.props.activity.picture} width="150px" height="150px"/>
+            <p> {this.props.activity.name} </p>
+        </div>
+        )
+    }
+    
+} 
+ 
 
 export default class City extends React.Component {
     constructor(props) {
@@ -34,10 +46,17 @@ export default class City extends React.Component {
         let city =  this.state.city;
         if(city == undefined){
             return ( <div>loading</div>)
-        }else {
+        }
+        else {
             return (
                 <div className='city'>
                     <img src={this.state.city.picture}/>
+                    <p>{this.state.city.name}</p>
+                    <p>{this.state.city.description}</p>
+                    <h1> Places </h1>
+                    {this.state.city.activities.filter(a => a.nature=='place').map((a,i) => <Activ activity={a}/> )}
+                    <h1> Events </h1>
+                    {this.state.city.activities.filter(a => a.nature=='event').map((a,i) => <Activ activity={a}/>)}
                 </div>
 
             )
