b40ed3b8bb3fcc911207f52b78cb9fde971433fb
julien.aballea <julien.aballea@104f05.iut-lens.univ-artois.fr>
2018-02-21 21:31:05 +0100

    headerbis

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-
+import Header from './Header.js';
 import Home from './Home.js';
 import City from './City.js';
 import Activity from './Activity.js';
@@ -9,22 +9,7 @@ import Modal from './Modal.js';
 
 ReactDOM.render(
     <div>
-        <header>
-            <nav className="navigation">
-                <div className="menuobjlogo"> <a href="/"><p><img src="/images/siteres/logo.png" width="250"/></p></a></div>
-                <ul>
-                    <li className="menuobj"><a className="bordermenu" href="/">Add a city</a></li>
-                    <li className="form"><form method="post" action="traitement.php">
-                        <input type="text" placeholder="Search"/>
-                        <input type="submit" value="Go" />
-                    </form></li>
-                    <li className="menuobj"><a className="bordermenu" id="signin" href="/">Sign In</a></li>
-                    <li className="menuobj" id="backgroundbutton"><a className="bordermenu" id="backgroundbutton" href="/">Log In</a></li>
-                </ul>
-                <div id="bar"><div id="filtre"></div></div>
-            </nav>
- 
-        </header>
+        <Header/>
         <Router history={hashHistory}>
             <Route path="/" component={Home}/>
             <Route path="/city/:id" component={City} />
